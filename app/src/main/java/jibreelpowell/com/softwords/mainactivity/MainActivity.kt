c3cdc0ec8220bf008a2e268b2b41247c236d872e
jibreel <jibreel.powell@gmail.com>
2021-02-14 12:09:37 -0600

    create child fragments for main activity bottom navigation

--- app/src/main/java/jibreelpowell/com/softwords/mainactivity/MainActivity.kt
+++ app/src/main/java/jibreelpowell/com/softwords/mainactivity/MainActivity.kt
@@ -1,8 +1,9 @@
 package jibreelpowell.com.softwords.mainactivity
 
-import androidx.databinding.DataBindingUtil
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import jibreelpowell.com.softwords.R
 import jibreelpowell.com.softwords.databinding.ActivityMainBinding
 import jibreelpowell.com.softwords.generate.GenerateComponent
@@ -11,13 +12,26 @@ import jibreelpowell.com.softwords.utils.app
 class MainActivity : AppCompatActivity() {
 
     lateinit var generateComponent: GenerateComponent
-    
-//    val bottomNavigationItemSelectedListener: BottomNavigationView
+
+    val bottomNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_generate -> {
+                    //TODO: create generate fragment
+                    true
+                }
+                R.id.navigation_history -> {
+                    //TODO: create history fragment
+                    true
+                }
+                else -> false
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         app.component.generateComponent().create().inject(this)
         super.onCreate(savedInstanceState)
-        val binding : ActivityMainBinding = DataBindingUtil.setContentView(
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
             this,
             R.layout.activity_main
         )
