adf8cb291cdec8c30072ffe36c2357a293da4c02
jibreel <jibreel.powell@gmail.com>
2021-03-02 19:07:57 -0600

    create viewModel in HistoryFragment via factory

--- app/src/main/java/jibreelpowell/com/softwords/history/HistoryFragment.kt
+++ app/src/main/java/jibreelpowell/com/softwords/history/HistoryFragment.kt
@@ -6,13 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import jibreelpowell.com.softwords.databinding.FragmentHistoryBinding
 import jibreelpowell.com.softwords.mainactivity.MainActivity
 import javax.inject.Inject
 
 class HistoryFragment : Fragment() {
 
-    @Inject lateinit var viewModel: HistoryViewModel
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private val viewModel: HistoryViewModel by viewModels {
+        viewModelFactory
+    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
