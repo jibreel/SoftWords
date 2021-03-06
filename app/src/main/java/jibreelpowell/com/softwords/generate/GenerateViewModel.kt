b65032a0ee4ca2bb75ee9b258ed26ee6d53f634e
jibreel <jibreel.powell@gmail.com>
2021-03-06 16:28:00 -0600

    fix crash on insert

--- app/src/main/java/jibreelpowell/com/softwords/generate/GenerateViewModel.kt
+++ app/src/main/java/jibreelpowell/com/softwords/generate/GenerateViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.ViewModel
 import jibreelpowell.com.softwords.generate.generator.Pattern
 import jibreelpowell.com.softwords.storage.GeneratedSentence
 import jibreelpowell.com.softwords.storage.SentenceDao
+import jibreelpowell.com.softwords.utils.scheduleCompletableInBackground
+import timber.log.Timber
 import javax.inject.Inject
 
 class GenerateViewModel @Inject constructor(private val sentenceDao: SentenceDao) :
@@ -27,21 +29,21 @@ class GenerateViewModel @Inject constructor(private val sentenceDao: SentenceDao
     }
 
     fun storeCurrentSentence() {
-//        sentence.value?.let {
-//            val generatedSentence = GeneratedSentence.newInstance(it)
-//            sentenceDao.insertAll(generatedSentence)
-//                .scheduleCompletableInBackground()
-//                .subscribeWith(object : DisposableCompletableObserver() {
-//                    override fun onComplete() {
-//                        storageResult.value = Result.success(generatedSentence)
-//                    }
-//
-//                    override fun onError(e: Throwable) {
-//                        Timber.e(e)
-//                        storageResult.value = Result.failure(e)
-//                    }
-//                })
-//        }
+        sentence.value?.let {
+            val generatedSentence = GeneratedSentence.newInstance(it)
+            sentenceDao.insertAll(generatedSentence)
+                .scheduleCompletableInBackground()
+                .subscribe(
+                    {
+                        Timber.d("Sentence inserted into database successfully")
+                        storageResult.value = Result.success(generatedSentence)
+                    },
+                    { e ->
+                        Timber.e(e)
+                        storageResult.value = Result.failure(e)
+                    }
+                )
+        }
     }
 
 }
