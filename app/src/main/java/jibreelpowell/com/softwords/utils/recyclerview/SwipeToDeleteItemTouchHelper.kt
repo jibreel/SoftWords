78b787a12f857d674b8a8939399a936096ee3150
jibreel <jibreel.powell@gmail.com>
2021-03-29 16:37:55 -0500

    create SwipeToDeleteItemTouchHelper

--- app/src/main/java/jibreelpowell/com/softwords/utils/recyclerview/SwipeToDeleteItemTouchHelper.kt
+++ app/src/main/java/jibreelpowell/com/softwords/utils/recyclerview/SwipeToDeleteItemTouchHelper.kt
@@ -5,6 +5,8 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffXfermode
+import android.graphics.Rect
+import android.graphics.drawable.ColorDrawable
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
@@ -14,7 +16,12 @@ class SwipeToDeleteItemTouchHelper(context: Context, deleteCallback: (index: Int
 
 class SimpleSwipeToDeleteCallback(context: Context, val deleteCallback: (index: Int) -> Unit): ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
 
+    private val deleteSweepIcon = ContextCompat.getDrawable(context, R.drawable.ic_delete_sweep)
+    private val iconIntrinsicWidth = deleteSweepIcon?.intrinsicWidth ?: 0
+    private val iconIntrinsicHeight = deleteSweepIcon?.intrinsicHeight ?: 0
+    private val background = ColorDrawable()
     private val backgroundColor = ContextCompat.getColor(context, R.color.recyclerview_swipe_to_delete_background)
+    private val clearPaint = Paint().apply { xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR) }
 
     override fun onMove(
         recyclerView: RecyclerView,
@@ -42,18 +49,31 @@ class SimpleSwipeToDeleteCallback(context: Context, val deleteCallback: (index: 
         val itemView = viewHolder.itemView
         val itemHeight = itemView.bottom - itemView.top
 
+        val bounds = Rect((itemView.right + dX).toInt(), itemView.top, itemView.right, itemView.bottom)
+
         //if swipe is cancelled
         if (dX == 0f && !isCurrentlyActive) {
             //clear canvas
-            c.drawRect(itemView.right + dX, itemView.top.toFloat(), itemView.right.toFloat(), itemView.bottom.toFloat(), clearPaint)
+            c.drawRect(bounds, clearPaint)
             super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
             return
         }
 
+        //draw the red delete background
+        background.color = backgroundColor
+        background.bounds = bounds
+        background.draw(c)
 
-    }
+        //Calculate delete icon position
+        val deleteIconMargin = (itemHeight - iconIntrinsicHeight) / 2
+        val deleteIconLeft = itemView.right - deleteIconMargin - iconIntrinsicWidth
+        val deleteIconTop = itemView.top + (itemHeight - iconIntrinsicHeight) / 2
+        val deleteIconRight = itemView.right - deleteIconMargin
+        val deleteIconBottom = deleteIconTop + iconIntrinsicHeight
 
-    companion object {
-        private val CLEAR_PAINT = Paint().apply { xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR) }
+        deleteSweepIcon?.setBounds(deleteIconLeft, deleteIconTop, deleteIconRight, deleteIconBottom)
+        deleteSweepIcon?.draw(c)
+
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
     }
 }
