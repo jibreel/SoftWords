ae2bd44b97d5ae0786b9dbb9c1105cc12497be2f
jibreel <jibreel.powell@gmail.com>
2021-03-29 17:44:45 -0500

    fix error deleting sentences and add test case for delete function

--- app/src/main/java/jibreelpowell/com/softwords/history/HistoryFragment.kt
+++ app/src/main/java/jibreelpowell/com/softwords/history/HistoryFragment.kt
@@ -8,8 +8,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
+import jibreelpowell.com.softwords.R
 import jibreelpowell.com.softwords.databinding.FragmentHistoryBinding
 import jibreelpowell.com.softwords.mainactivity.MainActivity
+import jibreelpowell.com.softwords.utils.recyclerview.SwipeToDeleteItemTouchHelper
 import javax.inject.Inject
 
 class HistoryFragment : Fragment() {
@@ -38,6 +41,27 @@ class HistoryFragment : Fragment() {
 
         binding.viewModel = viewModel
         binding.historyRecyclerView.adapter = adapter
+
+        context?.let {
+            val itemTouchHelper = SwipeToDeleteItemTouchHelper(it, viewModel::delete)
+            itemTouchHelper.attachToRecyclerView(binding.historyRecyclerView)
+        }
+
+        viewModel.deletionResult.observe(viewLifecycleOwner) { result ->
+            when {
+                result.isSuccess -> Snackbar.make(
+                    binding.root,
+                    R.string.snackbar_deleted_sentence,
+                    Snackbar.LENGTH_SHORT
+                ).show()
+                result.isFailure -> Snackbar.make(
+                    binding.root,
+                    R.string.snackbar_delete_sentence_failed,
+                    Snackbar.LENGTH_SHORT
+                )
+            }
+        }
+
         return binding.root
     }
 
--- app/src/main/java/jibreelpowell/com/softwords/history/HistoryViewModel.kt
+++ app/src/main/java/jibreelpowell/com/softwords/history/HistoryViewModel.kt
@@ -1,12 +1,35 @@
 package jibreelpowell.com.softwords.history
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import jibreelpowell.com.softwords.storage.GeneratedSentence
 import jibreelpowell.com.softwords.storage.SentenceDao
+import jibreelpowell.com.softwords.utils.SchedulerProvider
+import jibreelpowell.com.softwords.utils.scheduleCompletableInBackground
+import timber.log.Timber
 import javax.inject.Inject
 
 class HistoryViewModel @Inject constructor(
-    sentenceDao: SentenceDao
+    val sentenceDao: SentenceDao,
+    val schedulerProvider: SchedulerProvider
 ) : ViewModel()  {
 
     val allSentences = sentenceDao.loadAllSentences()
+    val deletionResult: MutableLiveData<Result<GeneratedSentence>> by lazy {
+        MutableLiveData()
+    }
+
+    fun delete(index: Int) {
+        val currentSentences = allSentences.value ?: arrayListOf()
+        val deletedSentence = currentSentences.getOrNull(index) ?: return
+
+        sentenceDao.delete(deletedSentence).scheduleCompletableInBackground(schedulerProvider).subscribeBy(
+            onComplete = { deletionResult.postValue(Result.success(deletedSentence)) },
+            onError = { e ->
+                Timber.e(e)
+                deletionResult.postValue(Result.failure(e))
+            }
+        )
+    }
 }
