1b46b4e60de1a455a43337e45542db50a344099c
jibreel <jibreel.powell@gmail.com>
2021-04-05 12:31:19 -0500

    wip

---
+++ app/src/main/java/jibreelpowell/com/softwords/history/detail/HistoryDetailFragment.kt
@@ -0,0 +1,6 @@
+package jibreelpowell.com.softwords.history.detail
+
+import androidx.fragment.app.Fragment
+
+class HistoryDetailFragment: Fragment() {
+}
---
+++ app/src/main/java/jibreelpowell/com/softwords/history/detail/HistoryDetailViewModel.kt
@@ -0,0 +1,37 @@
+package jibreelpowell.com.softwords.history.detail
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import jibreelpowell.com.softwords.storage.SentenceDao
+import jibreelpowell.com.softwords.utils.SchedulerProvider
+import timber.log.Timber
+import javax.inject.Inject
+
+class HistoryDetailViewModel @Inject constructor(
+    private val sentenceDao: SentenceDao,
+    private val schedulerProvider: SchedulerProvider
+) : ViewModel() {
+
+    val sentenceText = MutableLiveData<String>()
+    val sentenceTimestamp = MutableLiveData<String>()
+
+    var closeCallBack : () -> Unit = {
+        Timber.v("Close Callback was never set")
+    }
+
+    fun load() {
+        TODO("Load Sentence from DAO")
+    }
+
+    fun delete() {
+        TODO("Delete Sentence from DAO")
+    }
+
+    fun close() {
+        closeCallBack()
+    }
+
+    fun share() {
+        TODO("Open Share Intent with Sentence")
+    }
+}
