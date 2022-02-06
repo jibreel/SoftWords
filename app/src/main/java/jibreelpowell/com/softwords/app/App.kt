d16c06875ae25713915b4cc2abd48bfe9d1cb12c
Jibreel Powell <jibreel.powell@gmail.com>
2022-02-05 19:16:08 -0600

    Set up Koin modules

--- app/src/main/java/jibreelpowell/com/softwords/app/App.kt
+++ app/src/main/java/jibreelpowell/com/softwords/app/App.kt
@@ -5,16 +5,21 @@ import com.jakewharton.threetenabp.AndroidThreeTen
 import jibreelpowell.com.softwords.BuildConfig
 import jibreelpowell.com.softwords.di.AppComponent
 import jibreelpowell.com.softwords.di.DaggerAppComponent
+import jibreelpowell.com.softwords.di.koin.modules.Modules
 import jibreelpowell.com.softwords.storage.AppDatabase
 import jibreelpowell.com.softwords.utils.SchedulerProvider
+import org.koin.android.ext.android.inject
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 import timber.log.Timber
 import javax.inject.Inject
 
 class App: Application() {
     lateinit var component: AppComponent
 
-    @Inject lateinit var database: AppDatabase
-    @Inject lateinit var schedulerProvider: SchedulerProvider
+    lateinit var database: AppDatabase
+    lateinit var schedulerProvider: SchedulerProvider
 
     override fun onCreate() {
         super.onCreate()
@@ -23,10 +28,15 @@ class App: Application() {
             .factory()
             .create(applicationContext)
 
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(Modules.utilityModule, Modules.networkModule, Modules.storageModule)
+        }
+
         component.inject(this)
 
         database.checkInitialization(schedulerProvider)
-
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
---
+++ app/src/main/java/jibreelpowell/com/softwords/di/koin/modules/Modules.kt
@@ -0,0 +1,97 @@
+package jibreelpowell.com.softwords.di.koin.modules
+
+import jibreelpowell.com.softwords.BuildConfig
+import jibreelpowell.com.softwords.generate.generator.Word
+import jibreelpowell.com.softwords.network.linguarobot.LinguaRobotApiService
+import jibreelpowell.com.softwords.network.utils.RapidApiHttpInterceptor
+import jibreelpowell.com.softwords.network.words.WordsApiService
+import jibreelpowell.com.softwords.storage.*
+import jibreelpowell.com.softwords.utils.SchedulerProvider
+import jibreelpowell.com.softwords.utils.SchedulerProviderImpl
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.androidx.compose.get
+import org.koin.core.qualifier.named
+import org.koin.core.scope.get
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Modules {
+
+    val utilityModule = module {
+        single<SchedulerProvider> { SchedulerProviderImpl() }
+    }
+
+    val networkModule = module {
+        single<OkHttpClient> {
+            val interceptor = RapidApiHttpInterceptor()
+            val okBuilder =  OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+            if (BuildConfig.DEBUG) {
+                val loggingInterceptor = HttpLoggingInterceptor()
+                loggingInterceptor.level = HttpLoggingInterceptor.Level.HEADERS
+                okBuilder.addInterceptor(loggingInterceptor)
+            }
+            okBuilder.build()
+        }
+
+        single<Retrofit>(named("words")) {
+            val okHttpClient: OkHttpClient = get()
+            Retrofit.Builder()
+                .baseUrl(BuildConfig.WORDS_API)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .client(okHttpClient)
+                .build()
+        }
+
+        single<Retrofit>(named("linguaRobot")) {
+            val okHttpClient: OkHttpClient = get()
+            Retrofit.Builder()
+                .baseUrl(BuildConfig.LINGUA_ROBOT_API)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .client(okHttpClient)
+                .build()
+        }
+
+        single<WordsApiService> {
+            val retrofit: Retrofit by inject(qualifier = named("words"))
+            retrofit.create(WordsApiService::class.java)
+        }
+
+        single<LinguaRobotApiService> {
+            val retrofit: Retrofit by inject(qualifier = named("linguaRobot"))
+            retrofit.create(LinguaRobotApiService::class.java)
+        }
+    }
+
+    val storageModule = module {
+        single<AppDatabase> { params ->
+            val schedulerProvider: SchedulerProvider = get()
+            AppDatabase.getInstance(params.get(), schedulerProvider)
+        }
+
+        single<SentenceDao> {
+            val appDatabase: AppDatabase = get()
+            appDatabase.sentencesDao()
+        }
+
+        single<NounDao> {
+            val appDatabase: AppDatabase = get()
+            appDatabase.nounDao()
+        }
+
+        single<VerbDao> {
+            val appDatabase: AppDatabase = get()
+            appDatabase.verbDao()
+        }
+
+        single<PrepositionDao> {
+            val appDatabase: AppDatabase = get()
+            appDatabase.prepositionDao()
+        }
+    }
+}
